dd793ffa6ee5897ed50776f814a66d64283fb225
Stuart Berg <bergs@janelia.hhmi.org>
2018-11-27 14:03:25 -0500

    Hide FutureWarning from pandas.

--- flyemflows/__init__.py
+++ flyemflows/__init__.py
@@ -1,3 +1,9 @@
+import warnings
+## Don't show the following warning from within pandas:
+## FutureWarning: elementwise comparison failed; returning scalar instead, but in the future will perform elementwise comparison
+warnings.filterwarnings("ignore", module=r"pandas\..*", category=FutureWarning)
+
+
 # TODO:
 # DVIDSparkServices had a lot of sophisticated configuration in its __init__ file.
 # Some (or most) of it should be copied here.
