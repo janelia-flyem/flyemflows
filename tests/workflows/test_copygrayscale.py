772449da1531c05320fb077a73ee53c089bfecf1
Stuart Berg <bergs@janelia.hhmi.org>
2018-12-02 00:00:06 -0500

    test_copygrayscale: Add testcase for contrast-adjustment

--- tests/workflows/test_copygrayscale.py
+++ tests/workflows/test_copygrayscale.py
@@ -16,7 +16,7 @@ from flyemflows.bin.launchworkflow import launch_workflow
 TESTVOL_SHAPE = (256,256,256)
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def setup_dvid_grayscale_input(setup_dvid_repo):
     dvid_address, repo_uuid = setup_dvid_repo
  
@@ -45,8 +45,6 @@ def setup_dvid_grayscale_input(setup_dvid_repo):
             message-block-shape: [64,64,512]
             bounding-box: [[0,0,100], [256,200,256]]
  
-          #transpose-axes: ['1-y', 'x', 'z'] # XY_CLOCKWISE_90
-        
         output:
           dvid:
             server: {dvid_address}
@@ -71,7 +69,7 @@ def setup_dvid_grayscale_input(setup_dvid_repo):
     return template_dir, config, volume, dvid_address, repo_uuid, output_grayscale_name
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def setup_hdf5_grayscale_input(setup_dvid_repo):
     dvid_address, repo_uuid = setup_dvid_repo
     template_dir = tempfile.mkdtemp(suffix="copygrayscale-template")
@@ -97,8 +95,6 @@ def setup_hdf5_grayscale_input(setup_dvid_repo):
           geometry:
             message-block-shape: [64,64,256]
             bounding-box: [[0,0,100], [256,200,256]]
-
-          #transpose-axes: ['1-y', 'x', 'z'] # XY_CLOCKWISE_90
 
           # Enable multi-scale, since otherwise
           # Hdf5VolumeService doesn't support it out-of-the box
@@ -128,12 +124,13 @@ def setup_hdf5_grayscale_input(setup_dvid_repo):
     return template_dir, config, volume, dvid_address, repo_uuid, output_grayscale_name
 
 
-def _run_and_check_scale_0(setup):
+def _run(setup, check_scale_0=True):
     template_dir, config, volume, dvid_address, repo_uuid, output_grayscale_name = setup
 
     yaml = YAML()
     yaml.default_flow_style = False
     
+    # re-dump config in case it's been changed by a specific test
     with open(f"{template_dir}/workflow.yaml", 'w') as f:
         yaml.dump(config, f)
     
@@ -146,45 +143,60 @@ def _run_and_check_scale_0(setup):
     output_vol = fetch_raw(dvid_address, repo_uuid, output_grayscale_name, box_zyx)
     expected_vol = volume[box_to_slicing(*box_zyx)]
     
-    assert (output_vol == expected_vol).all(), \
-        "Written vol does not match expected"
+    if check_scale_0:
+        assert (output_vol == expected_vol).all(), \
+            "Written vol does not match expected"
     
     return box_zyx, expected_vol
 
 
 def test_copygrayscale_from_dvid_to_dvid(setup_dvid_grayscale_input):
-    _box_zyx, _expected_vol = _run_and_check_scale_0(setup_dvid_grayscale_input)
-  
-  
+    _box_zyx, _expected_vol = _run(setup_dvid_grayscale_input)
+   
+   
 def test_copygrayscale_from_hdf5_to_dvid(setup_hdf5_grayscale_input):
-    _box_zyx, _expected_vol = _run_and_check_scale_0(setup_hdf5_grayscale_input)
-
-
+    _box_zyx, _expected_vol = _run(setup_hdf5_grayscale_input)
+ 
+ 
 def test_copygrayscale_from_hdf5_to_dvid_multiscale(setup_hdf5_grayscale_input):
     _template_dir, config, _volume, dvid_address, repo_uuid, output_grayscale_name = setup_hdf5_grayscale_input
-    
+     
     # Modify the config from above to compute pyramid scales,
     # and choose a bounding box that is aligned with the bricks even at scale 2
     # (just for easier testing).
     config["input"]["geometry"]["bounding-box"] = [[0,0,0],[128,256,128]]
     config["copygrayscale"]["max-pyramid-scale"] = 2
     config["copygrayscale"]["pyramid-source"] = "compute-as-labels" # This test is easier to write if we use this downsampling method
-
-    box_zyx, scale_0_vol = _run_and_check_scale_0( setup_hdf5_grayscale_input )
-
+ 
+    box_zyx, scale_0_vol = _run( setup_hdf5_grayscale_input )
+ 
     scale_1_vol = downsample_labels(scale_0_vol, 2, True)
     scale_2_vol = downsample_labels(scale_1_vol, 2, True)
-
+ 
     # Check the other scales -- be careful to extract exactly one brick.
     output_vol = fetch_raw(dvid_address, repo_uuid, output_grayscale_name + '_1', box_zyx // 2)
     assert (output_vol == scale_1_vol).all(), \
         "Scale 1: Written vol does not match expected"
-
+ 
     # Check the other scales -- be careful to extract exactly one brick.
     output_vol = fetch_raw(dvid_address, repo_uuid, output_grayscale_name + '_2', box_zyx // 4)
     assert (output_vol == scale_2_vol).all(), \
         "Scale 2: Written vol does not match expected"
 
+
+def test_copygrayscale_from_hdf5_to_dvid_with_constrast_adjustment(setup_hdf5_grayscale_input):
+    """
+    Use the "constrast-adjustment" setting.
+    This test doesn't actually check the output, but at least it exercises the code enough to catch basic issues.
+    """
+    _template_dir, config, _volume, _dvid_address, _repo_uuid, _output_grayscale_name = setup_hdf5_grayscale_input
+
+    config["input"]["geometry"]["bounding-box"] = [[0,0,0],[256,128,128]]
+    config["copygrayscale"]["contrast-adjustment"] = "hotknife-destripe"
+    config["copygrayscale"]["hotknife-seams"] = [-1,50,100,150,256] # Note: These are X-coordinates.
+
+    _box_zyx, _scale_0_vol = _run( setup_hdf5_grayscale_input, check_scale_0=False )
+    
 
 if __name__ == "__main__":
     pytest.main(['-s', '--tb=native', '--pyargs', 'tests.workflows.test_copygrayscale'])
