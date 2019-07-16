9c3cf1a1190ffa51f4dbdafb5c425371d2363804
Stuart Berg <bergs@janelia.hhmi.org>
2019-07-16 18:09:19 -0400

    tests_n5_volume_service: Added a test for writing to non-aligned volume sizes

--- tests/volumes/test_n5_volume_service.py
+++ tests/volumes/test_n5_volume_service.py
@@ -5,7 +5,7 @@ import tempfile
 import pytest
 import numpy as np
 
-from neuclease.util import box_to_slicing
+from neuclease.util import box_to_slicing, boxes_from_grid
 
 from flyemflows.util.n5 import export_to_multiscale_n5
 from flyemflows.volumes import N5VolumeService
@@ -91,5 +91,42 @@ def test_multiscale_write(volume_setup):
         assert (orig_vol == written_vol).all(), f"Failed at scale {scale}"
 
 
+def test_multiscale_write_nonaligned(volume_setup):
+    """
+    Ensure that we can write to a volume whose overall shape is not a multiple of the chunkshape.
+    """
+    read_config, volume = volume_setup
+    reader = N5VolumeService(read_config)
+    
+    new_path = os.path.splitext(read_config["n5"]["path"])[0] + '_WRITE_TEST.n5'
+
+    config = copy.deepcopy(read_config)
+    config["n5"]["path"] = new_path
+    config["n5"]["dtype"] = str(volume.dtype)
+    config["n5"]["writable"] = True
+    
+    config["geometry"]["bounding-box"] = [[0,0,0], [250, 225, 200]]
+    config["geometry"]["available-scales"] = [*range(4)]
+    config["geometry"]["message-block-shape"] = [64,64,64]
+    
+    writer = N5VolumeService(config)
+
+    # WRITE
+    for scale in range(4):
+        full_box = writer.bounding_box_zyx // 2**scale
+        for box in boxes_from_grid(full_box, (128,128,64), clipped=True):
+            vol = reader.get_subvolume(box, scale)
+            writer.write_subvolume(vol, box[0], scale)
+
+    # READ
+    for scale in range(4):
+        full_box = writer.bounding_box_zyx // 2**scale
+        full_read = reader.get_subvolume(full_box, scale)
+        full_written = writer.get_subvolume(full_box, scale)
+        assert (full_read == full_written).all(), f"Failed at scale {scale}"
+
+
 if __name__ == "__main__":
-    pytest.main(['-s', '--tb=native', '--pyargs', 'tests.volumes.test_n5_volume_service'])
+    args = ['-s', '--tb=native', '--pyargs', 'tests.volumes.test_n5_volume_service']
+    #args += ['-k', 'multiscale_write_nonaligned']
+    pytest.main(args)
