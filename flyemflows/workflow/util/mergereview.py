68fadc6d2ba2aabaad1bbe55c30940f3d040af73
Stuart Berg <bergs@janelia.hhmi.org>
2019-11-05 17:17:28 -0500

    typereview assignment function has been moved to neuclease

--- flyemflows/workflow/util/mergereview.py
+++ flyemflows/workflow/util/mergereview.py
@@ -421,56 +421,6 @@ def generate_mergereview_assignments_from_df(server, uuid, instance, mr_fragment
     return all_tasks
 
 
-def generate_typereview_assignment(df, output_path, uuid='', comment=''):
-    """
-    Generate a mergereview-like assignment for doing type comparison.
-    
-    Args:
-        df:
-            A pandas DataFrame with columns ["body_a", "body_b", "score"]
-
-        output_path:
-            Where to write the output JSON assignment
-
-        uuid:
-            Optional.  Indicates which UUID was used to generate these tasks.
-        
-        comment:
-            Optional comment.
-    """
-    df = df.copy()
-    assert {*df.columns} >= {"body_a", "body_b", "score"}
-    
-    tasks = []
-    for row in df.itertuples():
-        task = {
-            # neu3 fields
-            'task type': "type review",
-            'task result id': f"{row.body_a}_{row.body_b}",
-            
-             "body ID A": row.body_a,
-             "body ID B": row.body_b,
-             'match_score': float(row.score),
-             
-            # Debugging fields
-            'debug': {
-                'original_uuid': uuid,
-                'comment': comment
-            }
-        }
-        tasks.append(task)
-
-    assignment = {
-        "file type":"Neu3 task list",
-        "file version":1,
-        "task list": tasks
-    }
-
-    with open(output_path, 'w') as f:
-        json.dump(assignment, f, indent=2)
-
-
-
 def pretty_print_assignment_json_items(items, f, cur_indent=0):
     """
     Python's standard pretty-print is quite ugly if your data involves lots of lists.
