33a3c84f9a2b61786b75feb3f39241e7ffc4e56e
Stuart Berg <bergs@janelia.hhmi.org>
2020-05-10 17:06:57 -0400

    VolumeServices: auto_retry only applies to HTTPErrors

--- flyemflows/volumes/brainmaps_volume_service.py
+++ flyemflows/volumes/brainmaps_volume_service.py
@@ -137,7 +137,8 @@ class BrainMapsVolumeServiceReader(VolumeServiceReader):
     # 2. If that fails due to 504 or 503 (probably cloud VMs warming up), wait 5 minutes and try again.
     @auto_retry(2, pause_between_tries=5*60.0, logging_name=__name__,
                 predicate=lambda ex: '503' in str(ex.args[0]) or '504' in str(ex.args[0]))
-    @auto_retry(3, pause_between_tries=60.0, logging_name=__name__)
+    @auto_retry(3, pause_between_tries=60.0, logging_name=__name__,
+                predicate=lambda ex: not isinstance(ex, HTTPError))
     def get_subvolume(self, box, scale=0):
         req_bytes = 8 * np.prod(box[1] - box[0])
         with self._resource_manager_client.access_context('brainmaps', True, 1, req_bytes):
