71298fc317b993779945983308885d334a613bd1
Stuart Berg <bergs@janelia.hhmi.org>
2023-08-11 10:30:29 -0400

    linting

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
+"""
+flyemflows setup.py
+"""
 from setuptools import find_packages, setup
 import versioneer
 
 # For now, requirements are only specified in the conda recipe, not here.
-# 
+#
 # TODO: Specify them here (or requirements.txt),
 #       and have the conda recipe import them via:
 #
@@ -11,22 +14,23 @@ import versioneer
 #       {% for dep in data['install_requires'] %}
 #      - {{ dep.lower() }}
 #      {% endfor %}
-# 
-setup( name='flyemflows',
-       version=versioneer.get_version(),
-       cmdclass=versioneer.get_cmdclass(),
-       description='Dask-based reconstruction tools working large image volumes, especially for DVID',
-       url='https://github.com/janelia-flyem/flyemflows',
-       packages=find_packages(exclude=('tests')),
-       package_data={'flyemflows': ['scripts/*']},
-       entry_points={
-          'console_scripts': [
-              'launchflow = flyemflows.bin.launchflow:main',
-              'init_dvid_bucket = flyemflows.bin.init_dvid_bucket:main',
-              'ingest_label_indexes = flyemflows.bin.ingest_label_indexes:main',
-              'erase_from_labelindexes = flyemflows.bin.erase_from_labelindexes:main',
-              'mesh_update_daemon = flyemflows.bin.mesh_update_daemon:main',
-              'roistats_table = flyemflows.workflow.util.roistats_table:main',
-          ]
-       }
-     )
+#
+setup(
+    name='flyemflows',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='Dask-based reconstruction tools working large image volumes, especially for DVID',
+    url='https://github.com/janelia-flyem/flyemflows',
+    packages=find_packages(exclude=('tests')),
+    package_data={'flyemflows': ['scripts/*']},
+    entry_points={
+        'console_scripts': [
+            'launchflow = flyemflows.bin.launchflow:main',
+            'init_dvid_bucket = flyemflows.bin.init_dvid_bucket:main',
+            'ingest_label_indexes = flyemflows.bin.ingest_label_indexes:main',
+            'erase_from_labelindexes = flyemflows.bin.erase_from_labelindexes:main',
+            'mesh_update_daemon = flyemflows.bin.mesh_update_daemon:main',
+            'roistats_table = flyemflows.workflow.util.roistats_table:main',
+        ]
+    }
+)
